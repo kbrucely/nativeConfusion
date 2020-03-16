3a5393c12d470afe6631c027f6f7599ffb187217
Kevin Ly <kevinly630@gmail.com>
2020-03-15 21:31:05 -0400

    fix missing line

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,4 @@
-. . .
-
+import React, { Component } from 'react';
 import { Text, ScrollView, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
